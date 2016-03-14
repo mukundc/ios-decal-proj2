c64d4d0bb0add0a01dfce5e33c9ecd01dfb911a8
Mukund Chillakanti <mukundc@berkeley.edu>
2016-03-13 20:51:29 -0700

    finished part 2

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -11,56 +11,127 @@ import UIKit
 class GameViewController: UIViewController {
 
     @IBOutlet weak var incorrectGuessesLabel: UILabel!
-    @IBOutlet weak var guessLabel: UITextField!
     @IBOutlet weak var hangmanWordLabel: UILabel!
     @IBOutlet weak var hangmanImage: UIImageView!
-    @IBOutlet weak var correctButton: UIButton!
-    @IBOutlet weak var incorrectButton: UIButton!
+
     
     //actions
     var incorrect = [Character]()
     var word = String()
+    var unchangedHangmanWord = String()
+    var gameOver: Bool = false
     
     
-    @IBAction func correctAction(sender: AnyObject) {
-        let guessedLetter = guessLabel.text!
-        let hangmanWord = hangmanWordLabel.text!
-        
-        let guess: Character = guessedLetter.characters[guessedLetter.characters.startIndex]
-        var newHangmanWord = ""
-        for i in self.word.characters.indices {
-            if self.word.characters[i] == guess {
-                print("we found a match")
-                newHangmanWord = newHangmanWord + String(guess)
-            }
-            else {
-                newHangmanWord = newHangmanWord + String(hangmanWord.characters[i])
-            }
-        }
-        guessLabel.text = ""
-        hangmanWordLabel.text = newHangmanWord
+    @IBAction func newGame(sender: AnyObject) {
+        gameOver = false
+        self.viewDidLoad()
+    }
+    @IBAction func restartGame(sender: AnyObject) {
+        gameOver = false
+        hangmanImage.image = UIImage(named: "hangman1.gif")
+        self.incorrect = [Character]()
+        hangmanWordLabel.text = unchangedHangmanWord
+        incorrectGuessesLabel.text = "Incorrect Guesses:"
+    }
+
+    @IBAction func guessLetter(sender: UIButton) {
+        guessActionButton(sender)
     }
     
+    func guessActionButton(button: UIButton) {
+        
+        if (!gameOver) {
+            let guessedLetter = button.titleLabel!.text
+            let hangmanWord = hangmanWordLabel.text!
+            var correct: Bool = false
+            
+            let guess: Character = guessedLetter!.characters[guessedLetter!.characters.startIndex]
+            
+            var newHangmanWord = ""
+            for i in self.word.characters.indices {
+                if self.word.characters[i] == guess {
+                    correct = true
+                    newHangmanWord = newHangmanWord + String(guess)
+                }
+                else {
+                    newHangmanWord = newHangmanWord + String(hangmanWord.characters[i])
+                }
+            }
+            hangmanWordLabel.text = newHangmanWord
+            
+            
+            
+            if(correct) {
+                if (hangmanWordLabel.text! == self.word) {
+                    gameWon()
+                }
+                
+            } else {
+                incorrectAction(button)
+            }
+        }
+    }
     
-    @IBAction func incorrectAction(sender: AnyObject) {
+   
+    func gameWon() {
+        gameOver = true
+        let alertController = UIAlertController(
+            title: "Game Over",
+            message: "YOU WON!",
+            preferredStyle: .Alert)
+    
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+    
+        alertController.addAction(cancelAction)
+    
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func gameLost() {
+        gameOver = true
+        let alertController = UIAlertController(
+            title: "Game Over",
+            message: "YOU LOST!",
+            preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+
+    }
+    
+    func incorrectAction(button: UIButton) {
         var incorrectLabelText = incorrectGuessesLabel.text!
-        let guessedLetter = guessLabel.text!
-        let guess: Character = guessedLetter.characters[guessedLetter.characters.startIndex]
+        let guessedLetter = button.titleLabel!.text
+        let guess: Character = guessedLetter!.characters[guessedLetter!.characters.startIndex]
         if !self.incorrect.contains(guess) {
             incorrectLabelText = incorrectLabelText + " " + String(guess)
             self.incorrect.append(guess)
         }
-        guessLabel.text = ""
         
-        let s = "hangman" + String(incorrect.count) + ".gif"
+        
+        let s = "hangman" + String(incorrect.count+1) + ".gif"
         hangmanImage.image = UIImage(named: s)
         incorrectGuessesLabel.text = incorrectLabelText
         
+        if (incorrect.count+1 >= 7) {
+            gameLost()
+        }
+        
     }
+    
+    @IBAction func backButton(sender: AnyObject) {
+         navigationController?.popViewControllerAnimated(true)
+    }
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.navigationItem.hidesBackButton = true
+        
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
         let phrase = hangmanPhrases.getRandomPhrase()
@@ -68,6 +139,8 @@ class GameViewController: UIViewController {
         
         hangmanImage.image = UIImage(named: "hangman1.gif")
         var hangmanWord = ""
+        self.word = ""
+        self.incorrect = [Character]()
         for i in phrase.characters.indices {
             if phrase[i] == " " {
                 hangmanWord = hangmanWord + "   "
@@ -79,6 +152,7 @@ class GameViewController: UIViewController {
 
             }
         }
+        unchangedHangmanWord = hangmanWord
         hangmanWordLabel.text = hangmanWord
         incorrectGuessesLabel.text = "Incorrect Guesses:"
 
